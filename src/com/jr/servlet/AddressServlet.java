12beee6097d4cbfb0fd56c29d4d8d95930c1f10f
Six_Wa <sixwa12138@qq.com>
2022-12-02 16:44:53 +0800

    邮寄地址AddressServlet

--- src/com/jr/servlet/AddressServlet.java
+++ src/com/jr/servlet/AddressServlet.java
@@ -1,11 +1,17 @@
 package com.jr.servlet;
 
+import com.google.gson.Gson;
+import com.jr.biz.impl.AddressBizImpl;
+import com.jr.entry.Address;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @Auther:唐一涵
@@ -25,10 +31,16 @@ public class AddressServlet extends HttpServlet {
         if (a==1){
             titleAndAddress(request,response);
         }else if (a==2){
-            message(request,response);
+            showAll(request,response);
         }else if (a==3){
-            invoice(request,response);
+            changeAddress(request,response);
         }else if (a==4){
+            changeDeAdd(request,response);
+        }else if (a==5){
+            addAddress(request,response);
+        }else if (a==6){
+            invoice(request,response);
+        }else if (a==7){
             detail(request,response);
         }
     }
@@ -45,19 +57,84 @@ public class AddressServlet extends HttpServlet {
         super.service(request, response);
     }
 
+
+    AddressBizImpl abi=new AddressBizImpl();
+
     public void titleAndAddress(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        int enterpriseId=Integer.parseInt(request.getParameter("enterpriseId"));
+        Address address=abi.queryDeAddByEId(enterpriseId,"A");
+        response.getWriter().print(new Gson().toJson(address));
+
     }
 
-    public void message(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void showAll(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int enterpriseId=Integer.parseInt(request.getParameter("enterpriseId"));
+        List<Address> list=abi.queryAddByEId(enterpriseId);
+        response.getWriter().print(new Gson().toJson(list));
 
     }
+
+    public void changeAddress(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Address address=new Address();
+        address.setAid(Integer.parseInt(request.getParameter("aid")));
+        address.setAddressee(request.getParameter("addressee"));
+        address.setPhone(request.getParameter("phone"));
+        address.setArea(request.getParameter("area"));
+        address.setAddressDetail(request.getParameter("addressDetail"));
+        boolean boo=abi.modifyAddress(address);
+        String str="失败";
+        if (boo){
+            System.out.println("success");
+        }else {
+            response.getWriter().print(new Gson().toJson(str));
+        }
+    }
+
+    public void changeDeAdd(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int aid=Integer.parseInt(request.getParameter("aid"));
+        int enterpriseId=Integer.parseInt(request.getParameter("enterpriseId"));
+        boolean boo=abi.modifyDeAddByEId(aid,enterpriseId);
+        String str="失败";
+        if (boo){
+            System.out.println("success");
+        }else {
+            response.getWriter().print(new Gson().toJson(str));
+        }
+    }
+
+    public void addAddress(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Address address=new Address();
+        address.setEnterpriseId(Integer.parseInt(request.getParameter("enterpriseId")));
+        address.setAddressee(request.getParameter("addressee"));
+        address.setPhone(request.getParameter("phone"));
+        address.setArea(request.getParameter("area"));
+        address.setAddressDetail(request.getParameter("addressDetail"));
+        boolean boo=abi.modifyAddress(address);
+        String str="失败";
+        if (boo){
+            System.out.println("success");
+        }else {
+            response.getWriter().print(new Gson().toJson(str));
+        }
+    }
+
 
     public void invoice(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Address> list=abi.queryAddByEId(Integer.parseInt(request.getParameter("enterpriseId")));
+        response.getWriter().print(new Gson().toJson(list));
 
     }
 
     public void detail(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Address address=abi.queryAddByIId(Integer.parseInt(request.getParameter("aid")));
+        response.getWriter().print(new Gson().toJson(address));
 
     }
 }
